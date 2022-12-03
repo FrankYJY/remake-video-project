cc0a2a37a2ce2d874fa7517742ee224edcdafb3e
Frank Yao <1404815414@qq.com>
2022-12-03 07:26:03 -0800

    others' implementation can run

--- video_rgb/opencv_implemented.py
+++ video_rgb/opencv_implemented.py
@@ -91,31 +91,36 @@ def dense_optical_flow(method, video_path, params=[], to_gray=False):
         old_frame = new_frame
 
 def main():
-    parser = ArgumentParser()
-    parser.add_argument(
-        "--algorithm",
-        choices=["farneback", "lucaskanade", "lucaskanade_dense", "rlof"],
-        required=True,
-        help="Optical flow algorithm to use",
-    )
-    parser.add_argument(
-        "--video_path", default="videos/cat.mp4", help="Path to the video",
-    )
+    # parser = ArgumentParser()
+    # parser.add_argument(
+    #     "--algorithm",
+    #     choices=["farneback", "lucaskanade", "lucaskanade_dense", "rlof"],
+    #     required=True,
+    #     help="Optical flow algorithm to use",
+    # )
+    # parser.add_argument(
+    #     "--video_path", default="videos/cat.mp4", help="Path to the video",
+    # )
 
-    args = parser.parse_args()
-    video_path = args.video_path
-    if args.algorithm == "lucaskanade":
-        lucas_kanade_method(video_path)
-    elif args.algorithm == "lucaskanade_dense":
-        method = cv2.optflow.calcOpticalFlowSparseToDense
-        dense_optical_flow(method, video_path, to_gray=True)
-    elif args.algorithm == "farneback":
-        method = cv2.calcOpticalFlowFarneback
-        params = [0.5, 3, 15, 3, 5, 1.2, 0]  # Farneback's algorithm parameters
-        dense_optical_flow(method, video_path, params, to_gray=True)
-    elif args.algorithm == "rlof":
-        method = cv2.optflow.calcOpticalFlowDenseRLOF
-        dense_optical_flow(method, video_path)
+    # args = parser.parse_args()
+    # # print(args)
+    # video_path = args.video_path
+    # if args.algorithm == "lucaskanade":
+    #     lucas_kanade_method(video_path)
+    # elif args.algorithm == "lucaskanade_dense":
+    #     method = cv2.optflow.calcOpticalFlowSparseToDense
+    #     dense_optical_flow(method, video_path, to_gray=True)
+    # elif args.algorithm == "farneback":
+    #     method = cv2.calcOpticalFlowFarneback
+    #     params = [0.5, 3, 15, 3, 5, 1.2, 0]  # Farneback's algorithm parameters
+    #     dense_optical_flow(method, video_path, params, to_gray=True)
+    # elif args.algorithm == "rlof":
+    #     method = cv2.optflow.calcOpticalFlowDenseRLOF
+    #     dense_optical_flow(method, video_path)
+
+    video_path = "C:/Users/14048/Desktop/multimedia/project/video_view/SAL_compacted.mp4"
+    method = cv2.optflow.calcOpticalFlowSparseToDense
+    dense_optical_flow(method, video_path, to_gray=True)
 
 
 if __name__ == "__main__":
